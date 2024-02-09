27c286d9d6b914c6269817ec8c1a1d67aae01773
roberto-palanga <r.palanga94@gmail.com>
2024-02-09 17:55:01 +0100

    Settiamo la data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
+
+let giornoDelGiudizio = new Date("Feb 12, 2024 09:30:00").getTime();
+
